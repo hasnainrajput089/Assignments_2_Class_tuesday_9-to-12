a40880dc7dfe4d1c48219b51bf004362d1dd1941
hasnainrajput089 <159238536+hasnainrajput089@users.noreply.github.com>
2024-02-28 13:17:24 -0800

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,34 @@
+
+//Data type in TypeScript
+
+//Example 1 :string
+const place :string = 'pakistan';
+
+//print place
+console.log(place);
+
+//Example 2 :Number
+const result :number = 99;
+
+//print result
+console.log(result);
+
+//Example 3 :Boolean
+const student :boolean = false;
+
+//print student
+console.log(student);
+
+//Example 4 :null
+const data :null =null;
+
+//print data
+console.log(data);
+
+//Example undefined
+const value =undefined;
+
+//print Undefined
+console.log(value);
+
+
