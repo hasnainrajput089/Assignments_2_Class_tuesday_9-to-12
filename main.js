c7f91375ebebd3337bc1d2c100fc478216cc6e4e
hasnainrajput089 <159238536+hasnainrajput089@users.noreply.github.com>
2024-02-28 13:20:34 -0800

    Data type

---
+++ main.js
@@ -0,0 +1,21 @@
+//Data type in TypeScript
+//Example 1 :string
+var place = 'pakistan';
+//print place
+console.log(place);
+//Example 2 :Number
+var result = 99;
+//print result
+console.log(result);
+//Example 3 :Boolean
+var student = false;
+//print student
+console.log(student);
+//Example 4 :null
+var data = null;
+//print data
+console.log(data);
+//Example undefined
+var value = undefined;
+//print Undefined
+console.log(value);
